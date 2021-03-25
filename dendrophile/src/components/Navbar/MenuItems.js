6c190af271160b2bf1a33e8422dfe0ab4af797d5
Rishit Bhojak <rajabhojak@gmail.com>
2021-03-25 20:08:58 +0530

    navbar

---
+++ dendrophile/src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+const MenuItems= [
+    {
+        title: "Home",
+        url: "#",
+        cName : "nav-links"
+    },
+    {
+        title: "Green Services",
+        url: "#",
+        cName : "nav-links"
+    },
+    {
+        title: "Green Products",
+        url: "#",
+        cName : "nav-links"
+    },
+    {
+        title: "About Us",
+        url: "#",
+        cName : "nav-links"
+    },
+    {
+        title: "Contact Us",
+        url: "#",
+        cName : "nav-links"
+    }
+]
+export default MenuItems;
